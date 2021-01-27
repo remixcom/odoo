22ba255ec8abe3a4a7f4d976a4bd32dbf9175217
hrodric <rportales7@gmail.com>
2021-01-27 15:59:21 -0300

    entrypoint base

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:=os.enriron.get('ODOO_DBHOST')}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=os.enriron.get('ODOO_DBPORT')}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=os.enriron.get('ODOO_DBUSER')}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=os.enriron.get('ODOO_DBPASSWORD')}}
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
 DB_ARGS=()
 function check_config() {
