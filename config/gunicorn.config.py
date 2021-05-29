5ab7b5fe3ec0ee1587bdbfcd815ba2562b39748d
hrodric <rportales7@gmail.com>
2021-05-28 22:19:24 -0300

    fix gunicorn setup base

--- config/gunicorn.config.py
+++ config/gunicorn.config.py
@@ -1,4 +1,4 @@
 command = '/opt/odoobeanstalk/v-odoobeanstalk/bin/gunicorn'
-pythonpath = '/opt/odoobeanstalk/odoo'
+pythonpath = '/opt/odoobeanstalk/v-odoobeanstalk/bin/python'
 bind = '0.0.0.0:8069'
 workers = 6
