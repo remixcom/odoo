91529383ced8f199b42495ff624cde1894221e50
hrodric <rportales7@gmail.com>
2021-05-29 16:21:39 -0300

    wsgi configs

--- config/gunicorn.config.py
+++ config/gunicorn.config.py
@@ -1,4 +1,2 @@
 command = '/opt/odoobeanstalk/v-odoobeanstalk/bin/gunicorn'
 pythonpath = '/opt/odoobeanstalk/v-odoobeanstalk/bin/python'
-bind = '0.0.0.0:8069'
-workers = 6
--- setup/odoo-wsgi_base.py
+++ setup/odoo-wsgi_base.py
@@ -13,12 +13,11 @@
 #   $ gunicorn odoo:service.wsgi_server.application -c openerp-wsgi.py
 
 import odoo
-import os
 
 #----------------------------------------------------------
 # Common
 #----------------------------------------------------------
-odoo.multi_process = True # Nah!
+odoo.multi_process = True  # Nah!
 
 # Equivalent of --load command-line option
 odoo.conf.server_wide_modules = ['base', 'web']
@@ -27,17 +26,14 @@ conf = odoo.tools.config
 # Path to the OpenERP Addons repository (comma-separated for
 # multiple locations)
 
-conf['addons_path'] = os.environ['ODOO_ADDONS']
+conf['addons_path'] = '../../addons/trunk,../../web/trunk/addons'
 
 # Optional database config if not using local socket
-if 'RDS_HOSTNAME' in os.environ:
-    conf['db_host'] = os.environ['RDS_HOSTNAME']
-    conf['db_port'] = os.environ['RDS_PORT'] # 5432
-    # conf['dbfilter'] = '^odoo11-.*$'
-    conf['db_user'] = os.environ['RDS_USERNAME']
-    conf['db_password'] = os.environ['RDS_PASSWORD']
-    conf['xmlrpc_port'] = os.environ['PORT']
-    conf['longpolling_port'] = os.environ['PORT1']
+# conf['db_name'] = 'mycompany'
+conf['db_host'] = 'odoo-prod.cspn35gmswug.us-east-1.rds.amazonaws.com'
+conf['db_user'] = 'odoobeanstalk'
+conf['db_port'] = 5432
+conf['db_password'] = 'StradivariuS'
 
 #----------------------------------------------------------
 # Generic WSGI handlers application
@@ -50,41 +46,10 @@ odoo.service.server.load_server_wide_modules()
 # Gunicorn
 #----------------------------------------------------------
 # Standard OpenERP XML-RPC port is 8069
-HOST = '0.0.0.0'
-bind = os.environ['HOST'] + ':' + os.environ['PORT']
+bind = '127.0.0.1:8069'
 pidfile = '.gunicorn.pid'
-ODOO_ADDONS = '/opt/odoobeanstalk/odoo/addons'
-RDS_HOSTNAME = 'odoo-prod.cspn35gmswug.us-east-1.rds.amazonaws.com'
-RDS_PORT = '5432'
-RDS_USERNAME = 'odoobeanstalk'
-RDS_PASSWORD = 'StradivariuS'
-PORT = '8069'
-
 workers = 6
-timeout = 720
-max_requests = 8000
-#############################
-#CUSTOM REMIX CONFIG
-#############################
-admin_passwd = 'StradivariuS'
-#############################
-logfile = '/var/log/odoobeanstalk/odoo.log'
-data_dir = '/mnt/efs'
-#############################
-proxy_mode = True
-xmlrpc_interface = '0.0.0.0'
-netrpc_interface = '0.0.0.0'
-#############################
-#############################
-#Multiprocessing Config
-#############################
-#800-1.5GB
-limit_memory_hard = 1572864000
-limit_memory_soft = 838860800
-limit_request = 8000
-limit_time_cpu = 600
-limit_time_real = 1200
-limit_time_real_cron = 3600
-max_cron_threads = 1
-workers = 6
-db_maxconn = 80
+# worker_class = 'sync'
+worker_connections = 8000
+timeout = 240
+max_requests = 2000
