8e4c254647be466e8a40d31bd18fecfd0a9691d4
hrodric <rportales7@gmail.com>
2021-05-30 22:30:49 -0300

    deploy config

--- odoo/service/wsgi_server.py
+++ odoo/service/wsgi_server.py
@@ -30,34 +30,6 @@ config['db_host'] = 'odoo-rds.cspn35gmswug.us-east-1.rds.amazonaws.com'
 config['db_user'] = 'odoobeanstalk'
 config['db_port'] = 5432
 config['db_password'] = 'StradivariuS'
-config['longpolling_port'] = 8070
-timeout = 720
-max_requests = 8000
-#############################
-#CUSTOM REMIX CONFIG
-#############################
-admin_passwd = 'StradivariuS'
-#############################
-logfile = '/var/log/odoobeanstalk/odoo.log'
-data_dir = '/mnt/efs'
-#############################
-proxy_mode = True
-xmlrpc_interface = '0.0.0.0'
-netrpc_interface = '0.0.0.0'
-#############################
-#############################
-#Multiprocessing Config
-#############################
-#800-1.5GB
-limit_memory_hard = 1572864000
-limit_memory_soft = 838860800
-limit_request = 8000
-limit_time_cpu = 600
-limit_time_real = 1200
-limit_time_real_cron = 3600
-max_cron_threads = 1
-workers = 6
-db_maxconn = 80
 
 _logger = logging.getLogger(__name__)
 
