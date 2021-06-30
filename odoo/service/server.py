f24b7734a52ec72f22b316d9e8eb6ece9d30146a
hrodric <rportales7@gmail.com>
2021-06-30 09:28:36 -0300

    test

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -21,6 +21,47 @@ import psutil
 import werkzeug.serving
 from werkzeug.debug import DebuggedApplication
 
+# AWS SDK Setup:
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "rds-rr-test-secret"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(
+                get_secret_value_response['SecretBinary'])
+
+
+
 if os.name == 'posix':
     # Unix only for workers
     import fcntl
